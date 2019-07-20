8482caee3fb8deb0b00ef18f31945d74ab05f983
Tim Trice <tim.trice@gmail.com>
2019-07-20 14:37:26 +0000

    Add knitr chunk labels

--- code/01_setup.R
+++ code/01_setup.R
@@ -1,3 +1,4 @@
+# ---- description ----
 #' Initiate DESCRIPTION
 holepunch::write_compendium_description(
   package = getOption("usethis.description")$Package,
@@ -5,16 +6,22 @@ holepunch::write_compendium_description(
   version = "0.0.0.9000"
 )
 
+# ---- license ----
 #' Set License
 usethis::use_mit_license()
 
+# ---- github-repo ----
 #' Add GitHub URL and Bug Reports
 usethis::use_github_links()
 
+# ---- move-holepunch ----
 #' Remove `holepunch` from Imports which is added due to the above call.
 desc::desc_del_dep("holepunch")
 
 #' Add Remote packages
 usethis::use_dev_package("holepunch")
 
+# ---- holepunch-write-dockerfile ----
+#' Create Dockerfile for "binder", This does not write the Dockerfile for 
+#' Docker hub (they use different images). 
 holepunch::write_dockerfile(r_date = "2019-07-01")
