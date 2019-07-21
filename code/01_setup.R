ef6a11e4d2c191d116063083aa2f6056f76c9a17
Tim Trice <tim.trice@gmail.com>
2019-07-21 08:06:38 -0500

    Describe better the calls

--- code/01_setup.R
+++ code/01_setup.R
@@ -1,5 +1,6 @@
 # ---- description ----
-#' Initiate DESCRIPTION
+#' Initiate DESCRIPTION. This effectively will rewrite the current 
+#' DESCRIPTION file with just basic info.
 holepunch::write_compendium_description(
   package = getOption("usethis.description")$Package,
   description = getOption("usethis.description")$Description,
@@ -7,21 +8,21 @@ holepunch::write_compendium_description(
 )
 
 # ---- license ----
-#' Set License
+#' Set License. See `?usethis::use_mit_license` for other options.
 usethis::use_mit_license()
 
 # ---- github-repo ----
-#' Add GitHub URL and Bug Reports
+#' Add repo URL and Bug Reports to DESCRIPTION
 usethis::use_github_links()
 
 # ---- move-holepunch ----
-#' Remove `holepunch` from Imports which is added due to the above call.
+#' `holepunch` will be listed under Depends. It is not yet available on CRAN so 
+#' this will generate an error. We have to move it to Remotes and add the repo 
+#' to Remotes putting the repo under Imports.
 desc::desc_del_dep("holepunch")
-
-#' Add Remote packages
 usethis::use_dev_package("holepunch")
 
 # ---- holepunch-write-dockerfile ----
-#' Create Dockerfile for "binder", This does not write the Dockerfile for 
-#' Docker hub (they use different images). 
+#' Create Dockerfile for [binder](https://gke.mybinder.org/), This does not 
+#' write the Dockerfile for Docker hub (they use different images). 
 holepunch::write_dockerfile(r_date = getOption("ew.build.date"))
